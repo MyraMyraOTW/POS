88772630cd8a4247c12cf3b9e300e4316ceb7908
Epirrus <dcter12@gmail.com>
2015-10-30 20:42:31 -0700

    last commit

--- app/src/main/java/com/example/myriad/pos/MainActivity.java
+++ app/src/main/java/com/example/myriad/pos/MainActivity.java
@@ -1,27 +1,20 @@
 package com.example.myriad.pos;
 
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Context;
-import android.os.Environment;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.EventLogTags;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.io.*;
-
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private double total;
+    private String pattern = "0.##";
+    private int ttype,taxchk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +35,132 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.b00).setOnClickListener(num);
         findViewById(R.id.dot).setOnClickListener(num);
 
+        findViewById(R.id.groc).setOnClickListener(op);
+        findViewById(R.id.auto).setOnClickListener(op);
+        findViewById(R.id.general).setOnClickListener(op);
+        findViewById(R.id.clear).setOnClickListener(op);
+        findViewById(R.id.gst).setOnClickListener(op);
+        findViewById(R.id.pst).setOnClickListener(op);
+        findViewById(R.id.total).setOnClickListener(op);
+        findViewById(R.id.refund).setOnClickListener(op);
+
+        TextView resultsstd = (TextView) findViewById(R.id.tview);
+        resultsstd.setTextColor(Color.parseColor("#ffe650"));
+
     }
 
     private View.OnClickListener num = new View.OnClickListener() {
         public void onClick(View v) {
             TextView resultsstd = (TextView) findViewById(R.id.tview);
             Button std = (Button)v;
-            resultsstd.append(std.getText());
+            resultsstd.append(std.getText().toString());
         }
     };
+
+    private View.OnClickListener op = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            DecimalFormat fmat = new DecimalFormat(pattern);
+            TextView resultsstd = (TextView) findViewById(R.id.tview);
+            double item, temp;
+            switch (v.getId()){
+                case R.id.groc:
+                    item = Double.parseDouble(resultsstd.getText().toString());
+                    switch (taxchk){
+                        case 1:
+                            temp = tax(item);
+                            item = temp;
+                            break;
+                        case 0:
+                            break;
+                    }
+                    total = total + item;
+                    resultsstd.setText("");
+                    taxchk = 0;
+                    break;
+                case R.id.auto:
+                    item = Double.parseDouble(resultsstd.getText().toString());
+                    switch (taxchk){
+                        case 1:
+                            temp = tax(item);
+                            item = temp;
+                            break;
+                        case 0:
+                            break;
+                    }
+                    total = total + item;
+                    resultsstd.setText("");
+                    taxchk = 0;
+                    break;
+                case R.id.general:
+                    item = Double.parseDouble(resultsstd.getText().toString());
+                    switch (taxchk){
+                        case 1:
+                            temp = tax(item);
+                            item = temp;
+                            break;
+                        case 0:
+                            break;
+                    }
+                    total = total + item;
+                    resultsstd.setText("");
+                    taxchk = 0;
+                    break;
+                case R.id.clear:
+                    resultsstd.setText("");
+                    taxchk = 0;
+                    break;
+                case R.id.gst:
+                    taxchk = 1;
+                    ttype = 1;
+                    break;
+                case R.id.pst:
+                    taxchk = 1;
+                    ttype = 2;
+                    break;
+                case R.id.total:
+                    resultsstd.setText(fmat.format(total));
+                    taxchk = 0;
+                    total = 0;
+                    break;
+                case R.id.refund:
+                    taxchk = 0;
+                    item = Double.parseDouble(resultsstd.getText().toString());
+                    total = total - item;
+                    resultsstd.setText("");
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        TextView resultsstd = (TextView) findViewById(R.id.tview);
+        outState.putString("totalValue", resultsstd.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        TextView resultsstd = (TextView) findViewById(R.id.tview);
+        resultsstd.setText(savedInstanceState.getString("totalValue"));
+    }
+
+    private double tax(double x){
+        double temp;
+        switch (ttype){
+            case 1:
+                temp = x * .05;
+                x = x + temp;
+                break;
+            case 2:
+                temp = x * .07;
+                x = x + temp;
+                break;
+        }
+        return x;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
